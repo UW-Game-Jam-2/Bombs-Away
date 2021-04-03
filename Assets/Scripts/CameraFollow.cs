62d98c946a6ed9cfb86fef0b411905db12f21142
fdunn1985 <fdunn1985@gmail.com>
2021-04-02 19:43:57 -0700

    updated changes with background, working on camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform followTransform;
+    public BoxCollider2D mapBounds;
+
+    private float xMin, xMax;
+    private float camX;
+    private float camOrthsize;
+    private float cameraRatio;
+    private Camera mainCam;
+    private Vector3 smoothPos;
+    public float smoothSpeed = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xMin = mapBounds.bounds.min.x;
+        xMax = mapBounds.bounds.max.x;
+        mainCam = GetComponent<Camera>();
+        camOrthsize = mainCam.orthographicSize;
+        cameraRatio = (xMax + camOrthsize) / 2.0f;
+        Debug.Log("xMin: " + xMin + ", xMax: " + xMax);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        Debug.Log(followTransform.position.x);
+        camX = Mathf.Clamp(followTransform.position.x, xMin + cameraRatio, xMax - cameraRatio);
+        smoothPos = Vector3.Lerp(this.transform.position, new Vector3(camX, followTransform.position.y, this.transform.position.z), smoothSpeed);
+        this.transform.position = smoothPos;
+    }
+}
