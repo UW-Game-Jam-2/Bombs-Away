b8109f4429b926973b9db15d7e4d7e9b4cad95c5
heavyMetalP0et <jarrod.luty@gmail.com>
2021-04-05 11:19:20 -0700

    Level Creation
    Created levels 1-3

--- Assets/Sprites/Scripts/ObjectivesManagerScript.cs
+++ Assets/Sprites/Scripts/ObjectivesManagerScript.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class ObjectivesManagerScript : MonoBehaviour
 {
@@ -14,27 +13,6 @@ public class ObjectivesManagerScript : MonoBehaviour
     private int currentLevel = 1;
     private int goldShotTarget = 10;
     private int silverShotTarget = 20;
-    private string levelGrade = "NO_GRADE";
-
-    [SerializeField]
-    private Canvas levelCompleteCanvas;
-    [SerializeField]
-    private TextMeshProUGUI coinText;
-    [SerializeField]
-    private TextMeshProUGUI chestText;
-    [SerializeField]
-    private TextMeshProUGUI bombText;
-    [SerializeField]
-    private TextMeshProUGUI overallText;
-
-    [SerializeField]
-    private TextMeshProUGUI coinObjectiveText;
-    [SerializeField]
-    private TextMeshProUGUI chestObjectiveText;
-    [SerializeField]
-    private TextMeshProUGUI bombObjectiveText;
-    [SerializeField]
-    private TextMeshProUGUI goalObjectiveText;
 
     public static ObjectivesManagerScript instance;
 
@@ -55,82 +33,32 @@ public class ObjectivesManagerScript : MonoBehaviour
         maxChestCount = GameManager.sharedInstance.GetMaxChestsByLevel(currentLevel);
         goldShotTarget = GameManager.sharedInstance.GetGoldShotTargetByLevel(currentLevel);
         silverShotTarget = GameManager.sharedInstance.GetSilverShotTargetByLevel(currentLevel);
-
-        coinObjectiveText.text = "0";
-        chestObjectiveText.text = "0/" + maxChestCount;
-        bombObjectiveText.text = "x0";
-        goalObjectiveText.text = "(" + goldShotTarget + ")";
-        goalObjectiveText.color = Color.yellow;
     }
 
     public void UpdateChestCount() {
         currentChestCount++;
-        chestObjectiveText.text = currentChestCount + "/" + maxChestCount;
+
         CheckAllObjectives();
+        Debug.Log("Chest count: " + currentChestCount);
     }
 
     public void UpdateCoinCount(int newCoinValue) {
         currentCoinCount += newCoinValue;
-        coinObjectiveText.text = currentCoinCount + "";
         CheckAllObjectives();
     }
 
     public void UpdateShotCount() {
         shotCount++;
-        bombObjectiveText.text = shotCount + "";
-
-        if (shotCount > goldShotTarget && shotCount <= silverShotTarget) {
-            goalObjectiveText.text = "(" + silverShotTarget + ")";
-            goalObjectiveText.color = Color.gray;
-        } else if (shotCount > silverShotTarget) {
-            goalObjectiveText.text = "(" + silverShotTarget + "+)";
-            goalObjectiveText.color = Color.magenta;
-        }
-    }
-
-    IEnumerator GameEndDelay(float timeToWait) {
-        yield return new WaitForSeconds(timeToWait);
-        SetLevelCompleteValues();
-    }
-
-    void SetLevelCompleteValues() {
-        levelCompleteCanvas.gameObject.SetActive(true);
-        coinText.text = currentCoinCount + "";
-        chestText.text = currentChestCount + "/" + maxChestCount;
-        bombText.text = "x" + shotCount;
-
-        levelGrade = "Overall: ";
-        if (shotCount <= goldShotTarget) {
-            levelGrade += "GOLD";
-        } else if (shotCount <= silverShotTarget) {
-            levelGrade += "SILVER";
-        } else {
-            levelGrade += "BRONZE";
-        }
-
-        overallText.text = levelGrade;
-
-        if (PlayerPrefs.HasKey("totalCoinCount")) {
-            PlayerPrefs.SetInt("totalCoinCount", PlayerPrefs.GetInt("totalCoinCount") + currentCoinCount);
-        } else {
-            PlayerPrefs.SetInt("totalCoinCount", currentCoinCount);
-        }
-
-        if (PlayerPrefs.HasKey("Level" + currentLevel + "_Grade")) {
-            string savedLevelGrade = PlayerPrefs.GetString("Level" + currentLevel + "_Grade");
-            if (savedLevelGrade.Equals("BRONZE") && (levelGrade.Equals("SILVER") || levelGrade.Equals("GOLD"))) {
-                PlayerPrefs.SetString("Level" + currentLevel + "_Grade", levelGrade);
-            } else if (savedLevelGrade.Equals("SILVER") && levelGrade.Equals("GOLD")) {
-                PlayerPrefs.SetString("Level" + currentLevel + "_Grade", levelGrade);
-            }
-        } else {
-            PlayerPrefs.SetString("Level" + currentLevel + "_Grade", levelGrade);
-        }
     }
 
     void CheckAllObjectives() {
         if (currentChestCount.Equals(maxChestCount) && currentCoinCount >= goalCoinCount) {
-            StartCoroutine(GameEndDelay(3f));
+            Debug.Log("Level " + currentLevel + " completed!");
+            Debug.Log("Coins Collected: " + currentCoinCount);
+            Debug.Log("Chests found: " + currentChestCount);
+            Debug.Log("Shots Taken: " + shotCount);
+            //TODO
+            //pop-up you win screen
         }
     }
 }
---
+++ Assets/Sprites/Scripts/ShipMovementScriptLevelSelect.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ShipMovementScriptLevelSelect : MonoBehaviour
+{
+
+    [SerializeField] Rigidbody2D rigidBody;
+    [SerializeField] float steeringPower;
+    [SerializeField] float movementAcceleration;
+    [SerializeField] SceneFader sceneFader;
+
+    public static event Action<string> didCollideWithIsland;
+    public static event Action didTriggerOpenOcean;
+
+
+    private float steeringAmount, speed, direction;
+    private Vector2 movement;
+
+    void Update()
+    {
+
+        steeringAmount = Input.GetAxis("Horizontal");
+        speed = - Input.GetAxis("Vertical") * movementAcceleration;
+        direction = Mathf.Sign(Vector2.Dot(rigidBody.velocity, rigidBody.GetRelativeVector(Vector2.up)));
+    } 
+
+    private void FixedUpdate()
+    {
+
+        float minimumTurning = rigidBody.velocity.magnitude;
+
+        rigidBody.rotation += steeringAmount * steeringPower * minimumTurning * direction * Time.fixedDeltaTime;
+
+        rigidBody.AddRelativeForce(Vector2.up * speed);
+
+        rigidBody.AddRelativeForce(Vector2.right * minimumTurning * steeringAmount / 2);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag != "Boundary")
+        {
+            didCollideWithIsland?.Invoke(collision.gameObject.name);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        didTriggerOpenOcean?.Invoke();
+    }
+}
